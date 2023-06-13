30a922479262cca6a1ac60e3669f3e64fcd9d07b
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2023-06-13 21:41:36 +0300

    Add files via upload

---
+++ src/utils/leaders.js
@@ -0,0 +1,36 @@
+const baseURL = "https://drive.google.com/uc?export=view&id=";
+
+const leaders = [
+  {
+    name: "Nemes Daniel",
+    role: "Pastor",
+    imgID: baseURL + "1JZeHet0fEbNUFKrIckVXuDeq_xMmgho0",
+  },
+  {
+    name: "Corseanschi Liviu",
+    role: "",
+    imgID: baseURL + "1x3manrggKanStYUXuWetGb_o0GrsjKKO",
+  },
+  {
+    name: "Stan Samuel",
+    role: "",
+    imgID: baseURL + "15C9z6RMlE4NVHLNWQ5w6SrOSEmGbqUGs",
+  },
+  {
+    name: "Zamfir Cornel",
+    role: "",
+    imgID: baseURL + "1nAWFjq7_zUoNDbQ0aqCPcyC5a16izMgJ",
+  },
+  {
+    name: "Matea Raul",
+    role: "",
+    imgID: baseURL + "1BHw_XYTWW1FkuVL0gyovt4S8a-V6zwA7",
+  },
+  {
+    name: "Samartinean Claudiu",
+    role: "",
+    imgID: baseURL + "1OScrkIxzc6HwTav4Y-eHdUBrECfQLRJS",
+  },
+];
+
+export default leaders;
