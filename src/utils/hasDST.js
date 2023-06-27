7cf2914247487fed3ff9c03e72f85bb37b913d94
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2023-06-27 18:17:50 +0300

    Delete src directory

--- src/utils/hasDST.js
+++
@@ -1,9 +0,0 @@
-const hasDST = () => {
-  const date = new Date();
-  const january = new Date(date.getFullYear(), 0, 1).getTimezoneOffset();
-  const july = new Date(date.getFullYear(), 6, 1).getTimezoneOffset();
-
-  return Math.max(january, july) !== date.getTimezoneOffset();
-};
-
-export default hasDST;
