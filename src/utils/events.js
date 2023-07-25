cab467b78aedc20bf35f4696b6dfbf23d4dda2bb
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2023-07-25 16:43:46 +0300

    Add files via upload

---
+++ src/utils/events.js
@@ -0,0 +1,28 @@
+const events = [
+  {
+    id: 1,
+    name: "Botez Nou Testamental",
+    date: "30.07.2023",
+    description:
+      "Cu autorul Domnului 15 suflete au incheiat un legamant cu Dumnezeu.",
+    pictures: [
+      "1UA7UDs3l0uEeCVRpKVAmgckP5f62T05R",
+      "11CghGdSD94pNLFj41OYU0Vc6OKfqPrPu",
+      "1OqnDps7pJ3LHGizuktMeS7sIELEBFN5a",
+      "1eirmjP6Zhz0TRQV4Viy3lTmNh8J0AIEl",
+    ],
+  },
+  {
+    id: 2,
+    name: "Aniversarea de 30 de ani",
+    date: "05.08.2023",
+    description: "Sarbatorim 30 de ani de la infiintarea Bisericii.",
+    pictures: [
+      "1V4Ntf4B0VJoEg5h5T7M4Alwp0Fa9_379",
+      "1j8uG7kHoF1ABvJcirD8Sgj39YF41l02g",
+      "163PVe6HVJyTUcZHILWZ9nVHc2f2JsVOT",
+    ],
+  },
+];
+
+export default events;
