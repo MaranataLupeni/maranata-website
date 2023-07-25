f5c9b1f40ed75a9d22fb1b3a2c3cd1da7edd6160
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2023-07-25 16:47:09 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,21 +13,28 @@ const Hero = (props) => {
         backgroundImage: `url(${bgImage})`,
       }}
     >
-      <h1 className={home && "home-hero-text"}>{text}</h1>
+      {text && <h1 className={home && "home-hero-text"}>{text}</h1>}
 
       {home && (
-        <table cellSpacing={0}>
-          <tbody>
-            <tr>
-              <td>Duminică 9:00 | {!dst ? "17:00" : "18:00"}</td>
-              <td>Joi {!dst ? "17:00" : "18:00"}</td>
-            </tr>
-            <tr>
-              <td>Marți 18:00</td>
-              <td>Vineri 18:00</td>
-            </tr>
-          </tbody>
-        </table>
+        <iframe src="https://www.youtube.com/embed/g1bDotfajFI?autohide=1&autoplay=1&controls=0&enablejsapi=1&iv_load_policy=3&loop=1&modestbranding=0&playsinline=1&rel=0&showinfo=0&wmode=opaque&mute=1&origin=https%3A%2F%2Fauthenticchurch.com&widgetid=1" />
+      )}
+
+      {home && (
+        <div className="home-info">
+          <h2>MARANATA DOMNUL NOSTRU VINE</h2>
+          <table cellSpacing={0}>
+            <tbody>
+              <tr>
+                <td>Duminică 9:00 | {!dst ? "17:00" : "18:00"}</td>
+                <td>Joi {!dst ? "17:00" : "18:00"}</td>
+              </tr>
+              <tr>
+                <td>Marți 18:00</td>
+                <td>Vineri 18:00</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
