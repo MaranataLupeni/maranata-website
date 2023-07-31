fc40780e7a1e2d738d709f20bb35feb7825755d4
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2023-07-31 15:06:08 +0300

    Create baptism2023.js

---
+++ src/utils/baptism2023.js
@@ -0,0 +1,307 @@
+const baseURL = "https://drive.google.com/uc?export=view&id=";
+const commonTitle = "Botez 2023";
+
+const baptism2023 = [
+  {
+    url: baseURL + "1RPtNGAgodZ8vP52HImEKccDnojGS8LMp",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1D_On3_s0BiSDBGA6z5ZW_oCOatt6XiZI",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1PU99JLR4IQmunkCoHrgXIkzR-94m1WiG",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1sIFhlBK17YnqpfW6Da_yjHzWMzfURJoK",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1HrEBpnO1P20Y2t6IO7HLSCKzcdjBgdws",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1Id7Kzie4w-MQmgc9J_FagmIXWkW7pLF2",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1pA2D-g0HCCdPVl6QmXcFbAub2VgRlTbl",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1V99dLkVoZlcXFV5aWjE5b0YB9No6wp7u",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1GpnbKgh6JBNPqKVjuDjm5G_qApNGrFNq",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1Vb5VXPqbEAZ-cwpUvujNVWksOWVdNRQc",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1jI1zVkdXoPPZGMaIGmtTtBYxsVvaTNLU",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1-NlI7Iubs2GMINSi3nuOe26tfr5HDO_U",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1T_mDIENXpnzL-oze4BLYXV1G1eFTfRu2",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1-qqtEGF-PFTRLZrpwivG9as8Lq5K1B6M",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1r0wUlxvpe0dE6iel4MghM5Nkm4uJQNBs",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1C2fA5NJEryCn0ZvqwFKkB8-xTh4z7Whj",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1AK2JMlSL91zMcJ7GprLBhQJBZ0f6T6fy",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1WOCvL016_kygUEdXrzG636S1VLzeVa2t",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1B0A1bgRkqhp08HoEraqC3qQ6zMetUP1D",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1z4RDlZCuDdJ1_L8avB2f3vuks6yt74Yv",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1dNI_DyX93IYQaHD_a7rKe1J5f8lD6_Ha",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1EJ2RE4AjyREivWkG_bBNqjuEOOqx9bgr",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1pZP1DduXHphQe0nfmIhsWycRZShCcX08",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1cuZ6qc1e9SpG7JHj-O-feuKo--ZyfzKJ",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1fK5JJl13AryOSZymhOtW0nXd5w0vZ1u5",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1GvJETwT_WwmxARUj0-3-plWs9PwFw0BO",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1XWmc4MKUVVsEwsH3n6sf1xOqx-MN7Hma",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1lvCmE8JkfJVPVKbTFPSUArXc1mD-fgUK",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1AFhIUL1S8p3HKb2R8Sr1nZq03lzRSa2Q",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1GPpD6m9nNr88vbd1_KTbryXMIjYSTdCX/",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1pqmG8PsvoOxuKnzR2ah1bEXtZPMIr8JT",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1gDeCQy8hZsbfcukAx_LD_x7dE3JA8Om4",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1dYn6r9E3ZeyC-6wHdejTQJrSqMxbVfIg",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1jnV7avDKbAlLnN_0GDvf1nXt5ijgzFYu",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "10tU2EBBRaJ_Jys4L6rCQ08A0z7e-MHsJ",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1pKAx4himKNBSanrecBQo409vCdU0kWV3",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1wDpsRhQHHf59ep3HyzGhuSMDyFLhqDBi",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1WbnMz-1-vAT16jG0bMcezr1mRdCIyeWT",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1C2BGlj-lJ-7_b1hUZOgnU1ClVkDyPcGI",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "13msvucriuy1dYxrGmqZ9rb7ZHCN2aG6c",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1sPirv-5gSUVxjV_LQd7rr7rxNQz1YMw7",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1Mos59HlxIro_bTHeehQf08V0u2i2yXbw",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1r6gEDm2ASWQs11Wnc_-jh6l-yqp5bue8",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1Dppi2uO-RY-rBGu1s6aWf342pUPleI7O",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1ukagi1HVHn8Ya1DfKOF8fOX9uPtfE64n",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1-zOBWc4utDW_vWkSXXC_8-3aaTci2A3I",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1WZXz9nQlzH87NYiVewPn63_Hu97bxo-r",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1jClTjBRrgoNE7kpcu63e9KO8DzjNq62J",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1-sq4hWWX8_txTuEMrKkbmxo78zndppD0",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1r96Ma2pTT1ABheinHq-IPS0cHB69RRTw",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1kV5TQO4wNf5M1j5MubTck5P8beqycoo5",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1MUc_vzEoNoXkbNm0W45yunUGcHAS3XhB",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1DWI_KSO4AlJM8JegrSw8yjBbkB2j1JbD",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1cceab39xP465pSBbZzgOWNK6Eage_SYh",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1aTuSQhJRleeDXJy41kF2onX1XE4fBt1D",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1mIZQJGtxxQfiPdAfUMUdzQIelagQCf3_",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1Ew7MgINSiP4xVp4W4p66O8YmddA52wNw",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1o7Dg_EZJ0L_ReAYQVzwbKyXk_P5vrESQ",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1uQHkWxdHS30W5_iE6Dbj1P-MDYvAITqz",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "15yCnbKS7LEu6m7qmZKUh3lmJJpfwuxVV",
+    title: commonTitle,
+    description: "",
+  },
+];
+
+export default baptism2023;
