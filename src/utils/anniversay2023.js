65e30f04df643890833665b67d7b686f897d66b0
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2024-01-05 16:48:27 +0200

    Add files via upload

---
+++ src/utils/anniversay2023.js
@@ -0,0 +1,12 @@
+const baseURL = "https://drive.google.com/uc?export=view&id=";
+const commonTitle = "Aniversarea a 30 de ani de la  înființarea Bisericii (2023)";
+
+const anniversary2023 = [
+  {
+    url: baseURL + "1NDiK2OE7LXN9CBnVEGYD83UAXP3CdBSs",
+    title: commonTitle,
+    description: "",
+  },
+]
+
+export default anniversary2023
---
+++ src/utils/new-year-2024.js
@@ -0,0 +1,62 @@
+const baseURL = "https://drive.google.com/uc?export=view&id=";
+const commonTitle = "Revelion 2024";
+
+const newYear2024 = [
+  {
+    url: baseURL + "19wy_qkexvZsQp2aMSRbxJKuktQt8EM3f",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1AFhxjAdr1VW21cDWX24a2mcr5nw4A9A5",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "1A7Rei8I60WazyoggTlSTmFA9QbJhnEqb",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19uUq_A2mJwk7zLN30uuYiblu8cCYkDdP",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19qg0radT1c5G_jI8rT4SnmeazMVR5hlr",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19i4bLs5agGgAofBtbhc8FUlLcNb-HCKx",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19eDcDkwS88_1QtoJQ3Yv2mmy94d4wyzv",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19a4BQAEmQc2bF0lzDrZKE8Ntk12sl112",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19ZuxEz2GoXNjN53SgO8hFwIWdOSd937q",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19VdNcGHZXpW6VjaARATo7wp5Dxaokc85",
+    title: commonTitle,
+    description: "",
+  },
+  {
+    url: baseURL + "19Ku9ccgnvsxtoi0caxdlw4fgcIEMv865",
+    title: commonTitle,
+    description: "",
+  },
+]
+
+export default newYear2024
