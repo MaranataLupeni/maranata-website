db0604869c0d1084cdd7b1105878de112de1fea2
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2024-01-05 12:09:42 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,20 @@ const Hero = (props) => {
   const { bgImage, home, text } = props;
   const dst = hasDST();
 
+  const changeTab = () => {
+    const buttonsContainer = document.querySelector(".buttons-container")
+    const programContainer = document.querySelector(".program-container")
+    if (!buttonsContainer.classList.contains("announcements-tab")) {
+      buttonsContainer.classList.add("announcements-tab")
+      programContainer.classList.add("not-display")
+      programContainer.style.pointerEvents = "none"
+    } else {
+      buttonsContainer.classList.remove("announcements-tab")
+      programContainer.classList.remove("not-display")
+      programContainer.style.pointerEvents = "unset"
+    }
+  }
+
   return (
     <div
       className="hero"
@@ -15,29 +29,49 @@ const Hero = (props) => {
     >
       {text && <h1 className={home && "home-hero-text"}>{text}</h1>}
 
-{/*       {home && (
-        <iframe src="https://www.youtube.com/embed/g1bDotfajFI?autohide=1&autoplay=1&controls=0&enablejsapi=1&iv_load_policy=3&loop=1&modestbranding=0&playsinline=1&rel=0&showinfo=0&wmode=opaque&mute=1&origin=https%3A%2F%2Fauthenticchurch.com&widgetid=1" />
-      )} */}
-
       {home && (
-        <div className="home-info">
-          <h2>MARANATA DOMNUL NOSTRU VINE</h2>
-          <table cellSpacing={0}>
-            <tbody>
-              <tr>
-                <td>Duminică 9:00 | {!dst ? "17:00" : "18:00"}</td>
-                <td>Joi {!dst ? "17:00" : "18:00"}</td>
-              </tr>
-              <tr>
-                <td>Marți 18:00</td>
-                <td>Vineri 18:00</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="program-container">
+            <h2>MARANATA DOMNUL NOSTRU VINE</h2>
+            <table cellSpacing={0}>
+              <tbody>
+                <tr>
+                  <td>Duminică 9:00 | {!dst ? "17:00" : "18:00"}</td>
+                  <td>Joi {!dst ? "17:00" : "18:00"}</td>
+                </tr>
+                <tr>
+                  <td>Marți 18:00</td>
+                  <td>Vineri 18:00</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      )}
+      {home && (
+        <div className="announcements">
+          <div className="buttons-container">
+            <button onClick={() => changeTab()}>Program</button>
+            <button onClick={() => changeTab()}>Anunțuri</button>
+          </div>
+          <div className="txt-container">
+            <h1>Conferință de Tineret</h1>
+            <p>Alătură-te nouă pentru a petrece o seară în prezența lui Dumnezeu, sau poate pentru a-L cunoaște pentru prima oară. Vino așa cum eșți, dar nu-ți putem garanta că vei pleca la fel 😊. Vom avea ca invitat pe Sorin Donțu, și de asemenea ai ocazia să cunoști persoane noi ✨, iubitoare ❤️, din tot județul 🗺️. P.S: la final... 🍔🥪☕🥤</p>
+
+            <span>
+              <i className="fa-solid fa-location-dot"></i>
+              <a href="https://www.google.com/maps/place/Biserica+Maranata/@45.3500689,23.1999991,15z/data=!4m6!3m5!1s0x474de7f5aa3fb301:0x3282b13d09e46019!8m2!3d45.3500689!4d23.1999991!16s%2Fg%2F1vg6sj6j?entry=ttu">
+                Biserica Maranata, Lupeni ⛪
+              </a>
+            </span>
+            <br />
+            <span>
+              <i className="fa-regular fa-clock"></i>
+              17:00
+            </span>
+          </div>
         </div>
       )}
     </div>
   );
 };
 
-export default Hero;
+export default Hero;  
