a7ba4d66ca238a5d402ffb5abebe00e105efa3cf
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2024-02-12 22:02:57 +0200

    Update announcements.js

--- src/utils/announcements.js
+++ src/utils/announcements.js
@@ -1,6 +1,6 @@
 const announcements = [
     {
-        title: "Conferință de Tineret",
+        title: "",
         description: "Alătură-te nouă pentru a petrece o seară în prezența lui Dumnezeu, sau poate pentru a-L cunoaște pentru prima oară. Vino așa cum eșți, dar nu-ți putem garanta că vei pleca la fel 😊. Vom avea ca invitat pe Sorin Donțu, și de asemenea ai ocazia să cunoști persoane noi ✨, iubitoare ❤️, din tot județul 🗺️. P.S: la final... 🍔🥪☕🥤",
         location: "Biserica Maranata, Lupeni ⛪",
         dateTime: "27.01.2024, ora 17:00"
