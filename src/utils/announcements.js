9ad82aa7b666f560d425b9b6899b7dee983503b7
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2024-03-07 21:59:34 +0200

    Update announcements.js

--- src/utils/announcements.js
+++ src/utils/announcements.js
@@ -1,9 +1,9 @@
 const announcements = [
     {
-        title: "",
-        description: "Alătură-te nouă pentru a petrece o seară în prezența lui Dumnezeu, sau poate pentru a-L cunoaște pentru prima oară. Vino așa cum eșți, dar nu-ți putem garanta că vei pleca la fel 😊. Vom avea ca invitat pe Sorin Donțu, și de asemenea ai ocazia să cunoști persoane noi ✨, iubitoare ❤️, din tot județul 🗺️. P.S: la final... 🍔🥪☕🥤",
+        title: "Dependețe care duc viața în ruină",
+        description: "Alătură-te nouă pentru a petrece o seară în prezența lui Dumnezeu, sau poate pentru a-L cunoaște pentru prima oară. Vino așa cum eșți, dar nu-ți putem garanta că vei pleca la fel 😊. Vom avea ca invitat pe Daniel Perijoc, și de asemenea ai ocazia să cunoști persoane noi ✨, iubitoare ❤️, din tot județul 🗺️. P.S: la final... 🍔🥪☕🥤",
         location: "Biserica Maranata, Lupeni ⛪",
-        dateTime: "27.01.2024, ora 17:00"
+        dateTime: "16.03.2024, ora 17:00"
     }
 ]
 
