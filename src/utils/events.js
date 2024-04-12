220da6e2719122eb7bcf8b2bcaa09939dd673de6
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2024-04-12 14:18:04 +0300

    Update events.js

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,4 +1,6 @@
 import baptism2023 from "./baptism2023";
+import anniversary2023 from "./anniversay2023";
+import newYear2024 from "./new-year-2024";
 
 const events = [
   {
@@ -13,12 +15,15 @@ const events = [
     id: 2,
     name: "Aniversarea de 30 de ani",
     date: "05.08.2023",
-    description: "Sarbatorim 30 de ani de la infiintarea Bisericii.",
-    pictures: [
-      "1V4Ntf4B0VJoEg5h5T7M4Alwp0Fa9_379",
-      "1j8uG7kHoF1ABvJcirD8Sgj39YF41l02g",
-      "163PVe6HVJyTUcZHILWZ9nVHc2f2JsVOT",
-    ],
+    description: "Am sarbatorit 30 de ani de la infiintarea Bisericii.",
+    pictures: anniversary2023,
+  },
+  {
+    id: 3,
+    name: "Revelion 2024 Tineret",
+    date: "31.12.2023",
+    description: "Am sarbatorit impreuna trecerea intre ani.",
+    pictures: newYear2024,
   },
 ];
 
