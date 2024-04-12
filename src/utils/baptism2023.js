3d0dbc22c664a4bb78fa584cee2f36ede04a3254
Biserica Maranata Lupeni <135708599+MaranataLupeni@users.noreply.github.com>
2024-04-12 13:50:07 +0300

    Update baptism2023.js

--- src/utils/baptism2023.js
+++ src/utils/baptism2023.js
@@ -1,299 +1,358 @@
-const baseURL = "https://drive.google.com/uc?export=view&id=";
+import img1 from "../assets/images/botez-2023/1.jpg"
+import img2 from "../assets/images/botez-2023/2.jpg"
+import img3 from "../assets/images/botez-2023/3.jpg"
+import img4 from "../assets/images/botez-2023/4.jpg"
+import img5 from "../assets/images/botez-2023/5.jpg"
+import img6 from "../assets/images/botez-2023/6.jpg"
+import img7 from "../assets/images/botez-2023/7.jpg"
+import img8 from "../assets/images/botez-2023/8.jpg"
+import img9 from "../assets/images/botez-2023/9.jpg"
+import img10 from "../assets/images/botez-2023/10.jpg"
+import img11 from "../assets/images/botez-2023/11.jpg"
+import img12 from "../assets/images/botez-2023/12.jpg"
+import img13 from "../assets/images/botez-2023/13.jpg"
+import img14 from "../assets/images/botez-2023/14.jpg"
+import img15 from "../assets/images/botez-2023/15.jpg"
+import img16 from "../assets/images/botez-2023/16.jpg"
+import img17 from "../assets/images/botez-2023/17.jpg"
+import img18 from "../assets/images/botez-2023/18.jpg"
+import img19 from "../assets/images/botez-2023/19.jpg"
+import img20 from "../assets/images/botez-2023/20.jpg"
+import img21 from "../assets/images/botez-2023/21.jpg"
+import img22 from "../assets/images/botez-2023/22.jpg"
+import img23 from "../assets/images/botez-2023/23.jpg"
+import img24 from "../assets/images/botez-2023/24.jpg"
+import img25 from "../assets/images/botez-2023/25.jpg"
+import img26 from "../assets/images/botez-2023/26.jpg"
+import img27 from "../assets/images/botez-2023/27.jpg"
+import img28 from "../assets/images/botez-2023/28.jpg"
+import img29 from "../assets/images/botez-2023/29.jpg"
+import img30 from "../assets/images/botez-2023/20.jpg"
+import img31 from "../assets/images/botez-2023/31.jpg"
+import img32 from "../assets/images/botez-2023/32.jpg"
+import img33 from "../assets/images/botez-2023/33.jpg"
+import img34 from "../assets/images/botez-2023/34.jpg"
+import img35 from "../assets/images/botez-2023/35.jpg"
+import img36 from "../assets/images/botez-2023/36.jpg"
+import img37 from "../assets/images/botez-2023/37.jpg"
+import img38 from "../assets/images/botez-2023/38.jpg"
+import img39 from "../assets/images/botez-2023/39.jpg"
+import img40 from "../assets/images/botez-2023/40.jpg"
+import img41 from "../assets/images/botez-2023/41.jpg"
+import img42 from "../assets/images/botez-2023/42.jpg"
+import img43 from "../assets/images/botez-2023/43.jpg"
+import img44 from "../assets/images/botez-2023/44.jpg"
+import img45 from "../assets/images/botez-2023/45.jpg"
+import img46 from "../assets/images/botez-2023/46.jpg"
+import img47 from "../assets/images/botez-2023/47.jpg"
+import img48 from "../assets/images/botez-2023/48.jpg"
+import img49 from "../assets/images/botez-2023/49.jpg"
+import img50 from "../assets/images/botez-2023/50.jpg"
+import img51 from "../assets/images/botez-2023/51.jpg"
+import img52 from "../assets/images/botez-2023/52.jpg"
+import img53 from "../assets/images/botez-2023/53.jpg"
+import img54 from "../assets/images/botez-2023/54.jpg"
+import img55 from "../assets/images/botez-2023/55.jpg"
+import img56 from "../assets/images/botez-2023/56.jpg"
+import img57 from "../assets/images/botez-2023/57.jpg"
+import img58 from "../assets/images/botez-2023/58.jpg"
+import img59 from "../assets/images/botez-2023/59.jpg"
+
 const commonTitle = "Botez 2023";
 
 const baptism2023 = [
   {
-    url: baseURL + "1RPtNGAgodZ8vP52HImEKccDnojGS8LMp",
+    url: img1,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1D_On3_s0BiSDBGA6z5ZW_oCOatt6XiZI",
+    url: img2,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1PU99JLR4IQmunkCoHrgXIkzR-94m1WiG",
+    url: img3,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1sIFhlBK17YnqpfW6Da_yjHzWMzfURJoK",
+    url: img4,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1Id7Kzie4w-MQmgc9J_FagmIXWkW7pLF2",
+    url: img5,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1pA2D-g0HCCdPVl6QmXcFbAub2VgRlTbl",
+    url: img6,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1V99dLkVoZlcXFV5aWjE5b0YB9No6wp7u",
+    url: img7,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1GpnbKgh6JBNPqKVjuDjm5G_qApNGrFNq",
+    url: img8,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1Vb5VXPqbEAZ-cwpUvujNVWksOWVdNRQc",
+    url: img9,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1jI1zVkdXoPPZGMaIGmtTtBYxsVvaTNLU",
+    url: img10,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1-NlI7Iubs2GMINSi3nuOe26tfr5HDO_U",
+    url: img11,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1T_mDIENXpnzL-oze4BLYXV1G1eFTfRu2",
+    url: img12,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1-qqtEGF-PFTRLZrpwivG9as8Lq5K1B6M",
+    url: img13,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1r0wUlxvpe0dE6iel4MghM5Nkm4uJQNBs",
+    url: img14,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1C2fA5NJEryCn0ZvqwFKkB8-xTh4z7Whj",
+    url: img15,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1AK2JMlSL91zMcJ7GprLBhQJBZ0f6T6fy",
+    url: img16,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1WOCvL016_kygUEdXrzG636S1VLzeVa2t",
+    url: img17,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1B0A1bgRkqhp08HoEraqC3qQ6zMetUP1D",
+    url: img18,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1z4RDlZCuDdJ1_L8avB2f3vuks6yt74Yv",
+    url: img19,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1dNI_DyX93IYQaHD_a7rKe1J5f8lD6_Ha",
+    url: img20,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1EJ2RE4AjyREivWkG_bBNqjuEOOqx9bgr",
+    url: img21,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1pZP1DduXHphQe0nfmIhsWycRZShCcX08",
+    url: img22,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1cuZ6qc1e9SpG7JHj-O-feuKo--ZyfzKJ",
+    url: img23,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1fK5JJl13AryOSZymhOtW0nXd5w0vZ1u5",
+    url: img24,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1GvJETwT_WwmxARUj0-3-plWs9PwFw0BO",
+    url: img25,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1XWmc4MKUVVsEwsH3n6sf1xOqx-MN7Hma",
+    url: img26,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1lvCmE8JkfJVPVKbTFPSUArXc1mD-fgUK",
+    url: img27,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1AFhIUL1S8p3HKb2R8Sr1nZq03lzRSa2Q",
+    url: img28,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1GPpD6m9nNr88vbd1_KTbryXMIjYSTdCX/",
+    url: img29,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1pqmG8PsvoOxuKnzR2ah1bEXtZPMIr8JT",
+    url: img30,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1gDeCQy8hZsbfcukAx_LD_x7dE3JA8Om4",
+    url: img31,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1dYn6r9E3ZeyC-6wHdejTQJrSqMxbVfIg",
+    url: img32,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1jnV7avDKbAlLnN_0GDvf1nXt5ijgzFYu",
+    url: img33,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "10tU2EBBRaJ_Jys4L6rCQ08A0z7e-MHsJ",
+    url: img34,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1pKAx4himKNBSanrecBQo409vCdU0kWV3",
+    url: img35,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1wDpsRhQHHf59ep3HyzGhuSMDyFLhqDBi",
+    url: img36,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1WbnMz-1-vAT16jG0bMcezr1mRdCIyeWT",
+    url: img37,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1C2BGlj-lJ-7_b1hUZOgnU1ClVkDyPcGI",
+    url: img38,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "13msvucriuy1dYxrGmqZ9rb7ZHCN2aG6c",
+    url: img39,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1sPirv-5gSUVxjV_LQd7rr7rxNQz1YMw7",
+    url: img40,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1Mos59HlxIro_bTHeehQf08V0u2i2yXbw",
+    url: img41,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1r6gEDm2ASWQs11Wnc_-jh6l-yqp5bue8",
+    url: img42,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1Dppi2uO-RY-rBGu1s6aWf342pUPleI7O",
+    url: img43,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1ukagi1HVHn8Ya1DfKOF8fOX9uPtfE64n",
+    url: img44,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1-zOBWc4utDW_vWkSXXC_8-3aaTci2A3I",
+    url: img45,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1WZXz9nQlzH87NYiVewPn63_Hu97bxo-r",
+    url: img46,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1jClTjBRrgoNE7kpcu63e9KO8DzjNq62J",
+    url: img47,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1-sq4hWWX8_txTuEMrKkbmxo78zndppD0",
+    url: img48,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1r96Ma2pTT1ABheinHq-IPS0cHB69RRTw",
+    url: img49,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1kV5TQO4wNf5M1j5MubTck5P8beqycoo5",
+    url: img50,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1MUc_vzEoNoXkbNm0W45yunUGcHAS3XhB",
+    url: img51,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1DWI_KSO4AlJM8JegrSw8yjBbkB2j1JbD",
+    url: img52,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1cceab39xP465pSBbZzgOWNK6Eage_SYh",
+    url: img53,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1aTuSQhJRleeDXJy41kF2onX1XE4fBt1D",
+    url: img54,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1mIZQJGtxxQfiPdAfUMUdzQIelagQCf3_",
+    url: img55,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1Ew7MgINSiP4xVp4W4p66O8YmddA52wNw",
+    url: img56,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1o7Dg_EZJ0L_ReAYQVzwbKyXk_P5vrESQ",
+    url: img57,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "1uQHkWxdHS30W5_iE6Dbj1P-MDYvAITqz",
+    url: img58,
     title: commonTitle,
     description: "",
   },
   {
-    url: baseURL + "15yCnbKS7LEu6m7qmZKUh3lmJJpfwuxVV",
+    url: img59,
     title: commonTitle,
     description: "",
   },
